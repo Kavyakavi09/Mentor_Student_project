b0a20d1a7da2108a7902075f61dbdbdb39462275
Kavya K <95955787+Kavyakavi09@users.noreply.github.com>
2022-06-26 14:53:59 +0530

    app created

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const connect = require('./connect_db');
+const studentRoute = require('./routes/student_route');
+const mentorRoute = require('./routes/mentor_route');
+
+// dotenv environment setup
+require('dotenv').config();
+
+// web server
+const app = express();
+app.use(express.json());
+
+// To connected with routes
+app.use('/api', studentRoute);
+app.use('/api', mentorRoute);
+
+let port = process.env.PORT || 4000;
+
+app.listen(port, async () => {
+  console.log(`The App is running on the port ${port}`);
+  // connect to the database
+  await connect();
+});
